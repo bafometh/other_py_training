f7aedafc45493770a97e61c420aec2efce90f28b
bafometh <zinevich.andrey@gmail.com>
2018-03-09 21:43:21 +0200

    c. Handles .txt files in following way:
    longest word

--- TestAssignment_v1.py
+++ TestAssignment_v1.py
@@ -8,8 +8,28 @@ user_path = input("path -> ")
 os.chdir(user_path)
 
 user_path = os.getcwd()
-print("Current dir %s" % user_path)
+print("Dir: %s" % user_path)
 
-f = open('text.txt','r')
+user_file = input("file -> ")
+print("File: %s" % user_file)
+f = open(user_file)
+theline = f.readline()
+for line in f.readlines():
+    print(line, end='')
+l = len(theline)
+m = 0
+ind = 0
+count = 0
+for i in range(l):
+    if theline[i] != ' ':
+        count += 1
+    else:
+        if count > m:
+            m = count
+            ind = i - count
+        count = 0
+if count > m:
+    m = count
+    ind = i - count + 1
+print('longest word:',theline[ind:ind + m])
 
-f.close()
