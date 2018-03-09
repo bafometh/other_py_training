db5e391f462f8c47a3d75465b899c4424160b30c
bafometh <zinevich.andrey@gmail.com>
2018-03-10 00:02:55 +0200

    Create console app that:
     a. Awaiting user input with a path to a folder
     b. Search across folder content for a files
     c. Handles .txt files in following way:
    - longest word
    - shortest word,
    - line length,
    - average word length.

--- TestAssignment_v1.py
+++ TestAssignment_v1.py
@@ -14,19 +14,31 @@ user_file = input("file -> ")
 print("File: %s" % user_file)
 f = open(user_file)
 theline = f.read()
-str = theline
-print(str)
-listWords = str.split()
+listWords = theline.split()
+lword=len(listWords)
+letter=len(theline)
+lletter=letter-1
+
 idLongestWord = 0
 idshortestWord = 0
+
 for i in range(1,len(listWords)):
     if len(listWords[idLongestWord]) < len(listWords[i]):
         idLongestWord = i
+
     else:
         if len(listWords[idshortestWord]) > len(listWords[i]):
             idshortestWord = i
-print('max: ',listWords[idLongestWord])
-print('min: ',listWords[idshortestWord])
+
+average=(lletter / len(listWords))
+lmax = len(listWords[idLongestWord])
+lmin = len(listWords[idshortestWord])
+print(theline)
+print('max: ',listWords[idLongestWord],' len: ',lmax, '\nMin: ',listWords[idshortestWord],' len: ',lmin,'\nWords: ', lword,'\nletter: ', lletter,' \nAverage :',average )
+
+
+
+
 
 
 
