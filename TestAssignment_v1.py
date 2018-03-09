3d0fa3012d059df2983aefc10eba781c20074a88
bafometh <zinevich.andrey@gmail.com>
2018-03-09 22:47:00 +0200

    c. Handles .txt files in following way:
     longest word
      shortest word,

--- TestAssignment_v1.py
+++ TestAssignment_v1.py
@@ -13,23 +13,21 @@ print("Dir: %s" % user_path)
 user_file = input("file -> ")
 print("File: %s" % user_file)
 f = open(user_file)
-theline = f.readline()
-for line in f.readlines():
-    print(line, end='')
-l = len(theline)
-m = 0
-ind = 0
-count = 0
-for i in range(l):
-    if theline[i] != ' ':
-        count += 1
+theline = f.read()
+str = theline
+print(str)
+listWords = str.split()
+idLongestWord = 0
+idshortestWord = 0
+for i in range(1,len(listWords)):
+    if len(listWords[idLongestWord]) < len(listWords[i]):
+        idLongestWord = i
     else:
-        if count > m:
-            m = count
-            ind = i - count
-        count = 0
-if count > m:
-    m = count
-    ind = i - count + 1
-print('longest word:',theline[ind:ind + m])
+        if len(listWords[idshortestWord]) > len(listWords[i]):
+            idshortestWord = i
+print('max: ',listWords[idLongestWord])
+print('min: ',listWords[idshortestWord])
+
+
+
 
